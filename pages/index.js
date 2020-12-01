659cceefa0f6b012b04a82e14870525198d135d9
mohamedeliwa <mohamed.av11@gmail.com>
2020-12-01 21:19:03 +0200

    adding asideNotes.md file to gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Head from "next/head";
 import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import { makeStyles } from "@material-ui/core/styles";
+
 
 
 export default function Home() {
